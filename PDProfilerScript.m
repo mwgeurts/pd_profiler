4a6f628365734ebc5a360cbb27b75cd1574fb6d4
Mark Geurts <mark.w.geurts@gmail.com>
2022-06-09 08:51:50 -0500

    Updated to use planned image instead of intensity profile

--- PDProfilerScript.m
+++ PDProfilerScript.m
@@ -1,5 +1,5 @@
 % Define input files
-targetIntensityProfileCDP = './10X_Open Field - 00_Intensity Profile.cdp';
+targetProfileDXF = './10X_PDIP.dxf';
 currentCalibrationCDP = './10X_COMPOSITE_20CM.cdp';
 measuredImageDXF = './TB1344_10X.dxf';
 
@@ -7,26 +7,37 @@ measuredImageDXF = './TB1344_10X.dxf';
 newCalibrationCDP = './10X_NEW_CALIBRATION.cdp';
 
 % Load and sort profiles from W2CAD files
-targetProfile = sortrows(LoadCDP(targetIntensityProfileCDP));
 currentCalibration = sortrows(LoadCDP(currentCalibrationCDP));
 
-% Load DXF measured image, clipping outer edges
+% Load target DXF image 
+[~, targetResolution, targetImage] = LoadDXF(targetProfileDXF);
+
+% Extract radial profile
+[x, y] = pol2cart(repmat(pi/4, [1 size(targetImage,1)]), ...
+            0:size(targetImage,1)-1);
+x = x + size(targetImage,2)/2 + 0.5;
+y = y + size(targetImage,1)/2 + 0.5;
+targetProfile = zeros(size(targetImage,1), 2);
+targetProfile(:,2) = interp2(targetImage, x, y, 'linear', NaN);
+targetProfile(:,1) = (0:size(targetProfile,1)-1)*targetResolution(1);
+
+% Load Measured DXF
 [~, measuredResolution, measuredImage] = LoadDXF(measuredImageDXF);
 measuredImage = measuredImage(11:end-11, 11:end-11);
 
 % Find symmetric center profile of measured image
 [~, measuredProfile(:,2)] = ImageSymmetricCenter(measuredImage);
-measuredProfile(:,1) = (0:length(measuredProfile)-1)*measuredResolution(1);
+measuredProfile(:,1) = (0:size(measuredProfile,1)-1)*measuredResolution(1);
 
 % Calculate new calibration profile from target/measured profile difference
 newProfile = currentCalibration;
-newProfile(:,4) = currentCalibration(:,4) .* interp1(targetProfile(:,1), ...
-    targetProfile(:,4), currentCalibration(:,1), 'linear', targetProfile(end,4)) ...
+newProfile(:,end) = currentCalibration(:,end) .* interp1(targetProfile(:,1), ...
+    targetProfile(:,end), currentCalibration(:,1), 'linear', targetProfile(end,end)) ...
     ./ interp1(measuredProfile(:,1), measuredProfile(:,2), ...
-    currentCalibration(:,1), 'linear', targetProfile(end,4));
+    currentCalibration(:,1), 'linear', targetProfile(end,end));
 
 % Renormalize to current calibration value
-newProfile(:,4) = newProfile(:,4) * currentCalibration(1,4)/newProfile(1,4);
+newProfile(:,end) = newProfile(:,end) * currentCalibration(1,end)/newProfile(1,end);
 
 % Save new calibration profile to W2CAD format, using header from current
 SaveCDP(newProfile, newCalibrationCDP, currentCalibrationCDP);
